d97c8d2f7789326bc03991a98e86850ea86dc4e5
Eric Timmerman <estimmerman@gmail.com>
2016-01-23 08:59:51 -0500

    formatting transaction data

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,5 @@
+var constants = require('../helpers/constants');
+
 /*
  * Plaid client
  */
@@ -19,9 +21,52 @@ exports.index = function(req, res, next) {
 				req.flash('errors', { msg: 'Unable to get transaction information.' });
 				return res.redirect('/home');
 			}
+
+			var expenses_data = {
+				total: 0.0
+			}
+
+			var category_data = {
+				'Other': { name: 'Other', total: 0.0, transactions: [] }
+			};
+
+			for (var i = 0; i < constants.BUDGET_CATEGORIES.length; i++) {
+				category_data[constants.BUDGET_CATEGORIES[i]] = { name: constants.BUDGET_CATEGORIES[i], total: 0.0, transactions: [] };
+			}
+
+			for (var i = 0; i < response.transactions.length; i++) {
+				var trans = response.transactions[i];
+				if (trans.category) {
+					var cat = trans.category[0];
+					category_data[cat].transactions.push(trans);
+				} else {
+					category_data.Other.transactions.push(trans);
+				}
+			}
+
+			for (var category in category_data) {
+				var total = 0.0;
+
+				if (!category_data.hasOwnProperty(category)) continue;
+
+				var single_category_data = category_data[category];
+				for (var i = 0; i < single_category_data.transactions.length; i++) {
+					var trans = single_category_data.transactions[i];
+					if (trans.amount) {
+						total += trans.amount;
+					}
+				}
+				single_category_data.total = total;
+				expenses_data.total += total;
+			}
+
+			console.log(category_data);
+			console.log(expenses_data);
+
 			return res.render('home', {
 				title: 'Home',
-				transactions: response.transactions
+				category_data: category_data,
+				expenses_data: expenses_data
 			});
 		});
 	} else {
---
+++ helpers/constants.js
@@ -0,0 +1,16 @@
+// file containing large constant variables/arrays
+module.exports = Object.freeze({
+    BUDGET_CATEGORIES : [
+	    'Bank Fees',
+		'Community',
+		'Food and Drink',
+		'Healthcare',
+		'Payment',
+		'Interest',
+		'Recreation',
+		'Service',
+		'Shops',
+		'Transfer',
+		'Travel'
+	],
+});
